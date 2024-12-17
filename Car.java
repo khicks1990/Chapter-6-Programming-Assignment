df4c5f5632ea8a8594c3288fc75bae859b7f2ddb
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-17 07:13:35 +0000

    Initial commit

---
+++ Car.java
@@ -0,0 +1,106 @@
+/**
+   The Car class stores data about a car
+   for the Car Class programming challenge.
+*/
+
+public class Car
+{
+   private int yearModel;  // The car's year model
+   private String make;    // The car's make
+   private int speed;      // The current speed
+
+   /**
+      The constructor initializes the car's
+      year model and make.
+      @param y The car's year model.
+      @param m The car's make.
+   */
+
+   Car(int y, String m)
+   {
+
+   }
+
+   /**
+      The setYearModel method sets the car's
+      year model.
+      @param y The car's year model.
+   */
+
+   public void setYearModel(int y)
+   {
+
+   }
+
+   /**
+      The setMake method sets the car's make.
+      @param m The car's make.
+   */
+
+   public void setMake(String m)
+   {
+
+   }
+
+   /**
+      The setSpeed method sets the car's current speed.
+      @param s The car's speed.
+   */
+
+   public void setSpeed(int s)
+   {
+
+   }
+
+   /**
+      The getYearModel method returns the car's
+      year model.
+      @return The car's year model.
+   */
+
+   public int getYearModel()
+   {
+
+   }
+
+   /**
+      The getMake method returns the car's make.
+      @return The car's make.
+   */
+   
+   public String getMake()
+   {
+
+   }
+
+   /**
+      The getSpeed method returns the car's
+      current speed.
+      @return The car's current speed.
+   */
+   
+   public int getSpeed()
+   {
+
+   }
+
+   /**
+      The accelerate method increases the car's
+      speed by 5 MPH.
+   */
+   
+   public void accelerate()
+   {
+
+   }
+
+   /**
+      The brake method decreases the car's
+      speed by 5 MPH.
+   */
+   
+   public void brake()
+   {
+
+   }
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+      Car sportsCar = new Car(2004, "Porsche");
+      
+      // Display the current status.
+      System.out.println("Current status of the car:");
+      System.out.println("Year model: " + sportsCar.getYearModel());
+      System.out.println("Make: " + sportsCar.getMake());
+      System.out.println("Speed: " + sportsCar.getSpeed());
+      
+      // Accelerate the car five times.
+      System.out.println("\nAccelerating...");
+      sportsCar.accelerate();
+      sportsCar.accelerate();
+      sportsCar.accelerate();
+      sportsCar.accelerate();
+      sportsCar.accelerate();
+      
+      // Display the speed.
+      System.out.println("Now the speed is " + sportsCar.getSpeed());
+      
+      // Brake the car five times.
+      System.out.println("\nBraking...");
+      sportsCar.brake();
+      sportsCar.brake();
+      sportsCar.brake();
+      sportsCar.brake();
+      sportsCar.brake();
+      
+      // Display the speed.
+      System.out.println("Now the speed is " + sportsCar.getSpeed());
+   }
+}
